935a4e453a7377ac1ec6a28e8047720ddecbc605
CodedreamerMonica <112541691+CodedreamerMonica@users.noreply.github.com>
2022-08-31 15:56:18 +0530

    Add files via upload

---
+++ NotemakingApp/Components/Notes.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is the Basics Of React </h1>
+      <p>
+        React is an efficient and flexible JavaScript library for building user
+        interfaces (and React itself is written using JavaScript). It breaks
+        down complex UIs into small, isolated code called “components”. By using
+        these components, React only concerns itself with what you see on the
+        front page of a website.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
